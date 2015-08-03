b8de274b5c113e3c8b234e0deb1f02662fbd93dd
Julio Maniratunga <mjb73w@gmail.com>
2015-08-04 01:21:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+LPF (Learn PHP Fast)
+--------------------
+
+PHP Website to be run with MAMP and exclusively used to learn the principles under the following book:
+
+    PHP: Learn PHP FAST - The Ultimate Crash Course to Learning
+          the Basics of the PHP Programming Language In No Time
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, minimum-scale=1.0">
+  <title>Learn PHP Fast</title>
+  <link rel="stylesheet" href="">
+</head>
+<body>
+<?php 
+  echo "Hello World";
+?>
+</body>
+</html>
