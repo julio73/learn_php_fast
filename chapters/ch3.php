a5ce7ab8ee47cc13f31291d8fb9573e281197fa9
Julio Maniratunga <mjb73w@gmail.com>
2015-08-04 04:33:34 +0200

    Finishing CH3.Exo1

--- chapters/ch3.php
+++ chapters/ch3.php
@@ -13,6 +13,31 @@
   <pre>
 <?php 
 
+/*
+Reproducing the following scenario with a variable:
+Value is now 8.
+Add 2. Value is now 10.
+Subtract 4. Value is now 6.
+Multiply by 5. Value is now 30.
+Divide by 3. Value is now 10.
+Increment value by one. Value is now 11.
+Decrement value by one. Value is now 10.
+*/
+$num = 8;
+print "Value is now $num.\n";
+$num += 2;
+print "Add 2. Value is now $num.\n";
+$num -= 4;
+print "Subtract 4. Value is now $num.\n";
+$num *= 5;
+print "Multiply by 5. Value is now $num.\n";
+$num /= 3;
+print "Divide by 3. Value is now $num.\n";
+$num++;
+print "Increment value by one. Value is now $num.\n";
+$num--;
+print "Decrement value by one. Value is now $num.\n";
+
  ?>
   </pre>
   
