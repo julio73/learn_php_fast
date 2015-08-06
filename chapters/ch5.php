9ceda9b1e3fe587c3b653a1f5e37ce17a56d2393
Julio Maniratunga <mjb73w@gmail.com>
2015-08-06 03:19:31 +0200

    Starting to read CH5

--- chapters/ch5.php
+++ chapters/ch5.php
@@ -1,3 +1,29 @@
 <title>Chapter 5</title>
 <a href=".." title="Home">Home</a>
-<h1>Soon...</h1>
+<h2>Chapter Five – Functions</h2>
+<pre>
+  Introduction
+  User Defined Functions
+    Function Arguments
+    Return values
+    Variable Scope
+  Pre-defined functions
+    Echo, Print, exit, die
+    String functions
+  Conclusion
+</pre>
+
+<!-- Exercice 1 -->
+<h2>Exercice 1</h2>
+<details>
+  <summary>Solution</summary>
+  <pre>
+<?php 
+
+
+
+ ?>
+  </pre>
+  
+</details>
+
