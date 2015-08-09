d8ba4796f9ba0d0385082c4eb33c00ed2ac8c8a1
Julio Maniratunga <mjb73w@gmail.com>
2015-08-09 09:25:46 +0200

    Prepping up CH6, reading soon

---
+++ chapters/ch6.php
@@ -0,0 +1,66 @@
+<?php
+  $title = "Chapter 6";
+  include_once 'shared/chapter_head.php';
+?>
+<body>
+<?php include_once 'shared/header.php'; ?>
+<h2>Chapter Six – Databases</h2>
+<pre>
+  Introduction
+  What is an API?
+    Connectors
+    Drivers
+    Extensions
+    PHP
+    MySQL
+    APIs
+    mysqli Extension
+    PDO Extension
+  PHPMyAdmin and getting familiar with MySQL Basics
+  MySQLi
+    Dual interface
+    Connections
+    Executing statements
+    Prepared statements
+</pre>
+
+<!-- Exercice 1 -->
+<h2 id="exo1">Exercice 1</h2>
+<details>
+  <summary>Solution | <a href="#" title="Back to top">▲ Top</a></summary>
+  <pre>
+<?php 
+
+
+
+ ?>
+  </pre>
+</details>
+
+<!-- Exercice 2 -->
+<h2 id="exo2">Exercice 2</h2>
+<details>
+  <summary>Solution | <a href="#" title="Back to top">▲ Top</a></summary>
+  <pre>
+<?php 
+
+
+
+ ?>
+  </pre>
+</details>
+
+<!-- Exercice 3 -->
+<h2 id="exo3">Exercice 3</h2>
+<details>
+  <summary>Solution | <a href="#" title="Back to top">▲ Top</a></summary>
+  <pre>
+<?php 
+
+
+
+ ?>
+  </pre>
+</details>
+<?php include 'shared/footer.php'; ?>
+</body>
