2a454d6ea091a2aaaf8b080f4c2e4e1056f08fcd
Julio Maniratunga <mjb73w@gmail.com>
2015-08-10 10:48:23 +0200

    Jumping to CH10 for OOP in PHP

---
+++ chapters/ch10.php
@@ -0,0 +1,63 @@
+<?php
+  $title = "Chapter 10";
+  include_once 'shared/chapter_head.php';
+?>
+<body>
+<?php include_once 'shared/header.php'; ?>
+<h2>Chapter Ten – Object Oriented Programming</h2>
+<pre>
+  Introduction
+  Basics
+    Class
+    new
+    extends
+    Properties
+    Constants
+    Autoloading Classes
+    Constructors and destructors
+    Object Inheritance
+    Scope Resolution
+    Operator (::)
+  Conclusion
+</pre>
+
+<!-- Exercice 1 -->
+<h2 id="exo1">Exercice 1</h2>
+<details>
+  <summary>Solution | <a href="#" title="Back to top">▲ Top</a></summary>
+  <pre>
+<?php 
+
+
+
+ ?>
+  </pre>
+</details>
+
+<!-- Exercice 2 -->
+<h2 id="exo2">Exercice 2</h2>
+<details>
+  <summary>Solution | <a href="#" title="Back to top">▲ Top</a></summary>
+  <pre>
+<?php 
+
+
+
+ ?>
+  </pre>
+</details>
+
+<!-- Exercice 3 -->
+<h2 id="exo3">Exercice 3</h2>
+<details>
+  <summary>Solution | <a href="#" title="Back to top">▲ Top</a></summary>
+  <pre>
+<?php 
+
+
+
+ ?>
+  </pre>
+</details>
+<?php include 'shared/footer.php'; ?>
+</body>
